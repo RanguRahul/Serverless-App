be1ae03d2622946d6e115da2707ec9d6682433f3
RahulRangu <rahulgoudpk@gmail.com>
2024-02-22 10:50:56 +0000

    new_conf

---
+++ WildRydesVue/config.js
@@ -0,0 +1,12 @@
+ndow._config = {
+	    cognito: {
+		            userPoolId: 'us-east-1_Z5Kd2kxvz',
+		            userPoolClientId: '2gmtgn5g7usijt8b4qa2450hco', 
+		            region: 'us-east-1'
+		        },
+	    api: {
+		            invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+		        }
+};
+
+
